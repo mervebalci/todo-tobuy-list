158ed8a7047dfdf54fdc15e8915dacf615d2bf40
Merve Balci <mervebalci090@gmail.com>
2023-12-02 20:21:08 -0400

    Functioning the Add Button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+
+let counter = 0;
 
 export default function App() {
+  const[tasks, setTasks] = useState([]);
+
+  function addTask() {
+    const userInput = document.getElementById('userInput').value;
+
+    const task = {id: counter, name: userInput};
+
+    setTasks([...tasks, task]);
+
+    counter = counter + 1;
+
+    document.getElementById('userInput').value = "";
+    console.log(tasks)
+  }
+
   return (
     <div className="App">
       <h1>TODO LIST</h1>
-      <input id="userInput" placeholder="Add a new to do..."></input>
-      <button>Add</button>
+      <input id="userInput" placeholder="Add a new to do"></input>
+      <button onClick={addTask}>Add</button>
     </div>
   );
 }
